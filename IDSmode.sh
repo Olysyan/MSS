48b25dbb21f784e7167b57c8215553488f3948c8
Olysyan <dmitriy.coltakov@gmail.com>
2021-09-27 23:57:55 +0300

    rm

--- IDSmode.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo snort -c /usr/local/etc/snort/snort.lua -R /etc/snort3-community-rules/snort3-community.rules -i `ifconfig | cut -d ' ' -f1| tr ':' '\n' | awk NR==1` -A alert_fast -s 65535 -k none
